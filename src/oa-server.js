ed08caeb711d4f00f1bbea59d29eedbf194fcb8d
Ari Siitonen <jalopuuverstas@gmail.com>
2022-07-31 20:20:14 +0300

    cp

--- src/oa-server.js
+++ src/oa-server.js
@@ -123,16 +123,19 @@ if (argv.init_conf) {
   console.log("Copying Template Config", src, "=>", target);
   fs.copyFileSync(src, target)
   for (var dom of fs.readdirSync(key_src)) {
-    var pp = path.join(key_src, dom)
+    var ps = path.join(key_src, dom)
+    var pp = path.join(key_p, dom)
+    console.log("Checking Domain Path", pp);
     if (!fs.existsSync(pp)) {
       fs.mkdirSync(pp)
       console.log("Created Path", pp);
     }
     console.log("Copying Keyfiles from domain", dom);
-    for (var f of fs.readdirSync(pp)) {
-      var ff = path.join(pp, f)
+    for (var f of fs.readdirSync(ps)) {
+      var ff = path.join(ps, f)
       var tt = path.join(key_p, dom, f)
       console.log("Copying Template Config", ff, "=>", tt);
+      fs.copyFileSync(ff, tt)
     }
   }
 }
